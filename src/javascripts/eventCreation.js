5e9b60d7b78c7f2352ebb00fb03f6774a2e89cf5
Yuichi Hagio <yhagio87@gmail.com>
2015-11-23 22:19:51 -0500

    resetInnerHTML() helper fn in register

--- src/javascripts/eventCreation.js
+++ src/javascripts/eventCreation.js
@@ -1,22 +1,5 @@
 'use strict';
 
-// Set attrubute to a specific element
-// Param 'elementID' : element's id
-// Param 'attr' : attribute type
-// Param 'value' : attribute value
-function setAttribute(elementID, attr, value){
-  return document.getElementById(elementID).setAttribute(attr, value);
-}
-
-// Check if the browser supports 'datetime-local' attribute
-// If not, use datetimepicker library to compensate
-// the difficulty of entering the date + time
-function supportThisTypeAttribute(type) {
-  var input = document.createElement("input");
-  input.setAttribute("type", type);
-  return input.type == type;
-}
-
 if(!supportThisTypeAttribute('datetime-local')){
   setAttribute('startDate', 'type', 'text');
   setAttribute('startDate', 'data-date-format', 'yyyy-mm-dd hh:ii');
